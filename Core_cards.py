5ec1bd5e19ebb60289722aa4846b326938df8b20
David Kiesel-Nield <query42@gmail.com>
2018-05-21 14:35:03 -0700

    Added first game ten cards as subclasses of Card

---
+++ Core_cards.py
@@ -0,0 +1,30 @@
+class Smithy(Card):
+    def action(self, player):
+        player.hand += draw_cards(3)
+
+class Cellar(Card):
+    pass
+
+class Village(Card):
+    pass
+
+class Thief(Card):
+    pass
+
+class Market(Card):
+    pass
+
+class Woodcutter(Card):
+    pass
+
+class Remodel(Card):
+    pass
+
+class Workshop(Card):
+    pass
+
+class Mine(Card):
+    pass
+
+class Library(Card):
+    pass
