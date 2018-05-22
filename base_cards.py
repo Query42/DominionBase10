fd945be031a09c16067172f76c3d4384d3dbaac2
Dave K-N <query42@gmail.com>
2018-05-21 19:35:54 -0700

    Fixed/modified cards module
    -Fixed card class init
    -Renamed args and attributes to conform with naming conventions
    -Added coin value attribute (for treasures, etc.)

--- base_cards.py
+++ base_cards.py
@@ -1,6 +1,10 @@
 class Card():
-    def init_(self, cardtype, cardsubtype, costtobuy, vpvalue):
-        self.cardtype = cardtype
-        self.cardsubtype = cardsubtype
-        self.cardcost = costtobuy
-        self.cardvpvalue = vpvalue
+    def __init__(
+        self, card_types, buy_cost, subtype=None, vp_value=None, coin_value=None
+        ):
+        self.types = card_types
+        self.cost = buy_cost
+        self.subtype = subtype
+        self.vp_value = vp_value
+        self.coin_value = coin_value
+        
