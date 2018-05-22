fddfeb0f5d507a2fbcc800706926715707e74d63
Dave K-N <query42@gmail.com>
2018-05-21 19:24:23 -0700

    Modified init
    -Cleaned up naming conventions in player class and player init.
    -Merged player class into player init.
    -Fixed init function to avoid making strings variable names.
    -Changed init module to run and demonstrate init when run independently.
    -Added python cache files to gitignore.

--- Dominion_player_init.py
+++ Dominion_player_init.py
@@ -1,23 +1,45 @@
-import Dominion_player_class
-import Dominion_base_card_class
 import time
 import random
 
-def Player_init():
-    NumberofPlayers = 0 #declare the number of players to 0
-    PlayerList = []
-    while NumberofPlayers<4: #loop until there are 4 players
-        NumberofPlayers = NumberofPlayers + 1 #increment NumberofPlayers
-        print("What is the ", NumberofPlayers,"st player's name?")
-        PlayerName = input("??")
-        PlayerName = Dominion_player_class.Player(PlayerName)
-        #Player PlayerName #declare Player classes with Dominion_classes.py
-        PlayerList.append(PlayerName)
-    random.shuffle(PlayerList)
-    return PlayerList
+import Dominion_base_card_class
 
 
-#Player_list = Player_init()
+class Player():
+    """Player object. Tracks states of individual players."""
+    def __init__(self, name):
+        self.name = name
+        self.deck = ['Estate', 'Estate', 'Estate', 'Copper', 'Copper', 'Copper', 'Copper', 'Copper', 'Copper', 'Copper']
+        self.handsize = 5
+        self.turncount = 0
+        self.activeturn = 0
+        self.baseactions = 1
+        self.remainingactions = 0
+        self.gained_ = []
 
+def player_init():
+    """
+    Prompt for names of four players. Create player objects.
+    Randomize turn order. Return randomized list of player objects.
+    """
+    player_count = 0 #declare the number of players to 0
+    player_list = []
+    while player_count<4: #loop until there are 4 players
+        player_count = player_count + 1 #increment player_count
+        print("What is the name of player {}?".format(player_count))
+        #I changed the above input prompt to avoid "4st"
+        player_name = input("> ")
+        #player_name = Player(player_name)
+        #Line above: this is going to throw an error as you're trying to assign a
+        #string as a variable name. Changing implementation
+        #Player player_name #declare Player classes with Dominion_classes.py
+        #player_list.append(player_name)
+        player_list.append(Player(player_name))
+        #This implementation just adds the new player object to the player list. We
+        #can examine it by calling for the player object's names if necessary.
+    random.shuffle(player_list)
+    return player_list
 
+player_list = player_init()
 
+for player_obj in player_list:
+    print(player_obj.name)
