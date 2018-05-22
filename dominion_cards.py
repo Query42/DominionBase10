48c888f7555718c61758148076250c0d2315abc8
Dave K-N <query42@gmail.com>
2018-05-21 21:57:57 -0700

    Added Cellar, draw, shuffle
    -Added discard_pile attribute to Player class.
    -Added draw_cards function to Dominion.py.
    -Added shuffle function to Dominion.py.
    -Added Cellar functionality.

--- dominion_cards.py
+++ dominion_cards.py
@@ -1,10 +1,42 @@
+from base_cards import Card
+
 class Smithy(Card):
-    def action(self, player):
-        draw_cards(player, 3)
+    def action(player):
+        player.hand += draw_cards(player, 3)
         return
 
 class Cellar(Card):
-    pass
+    def action(player):
+        debug_count = len(player.hand)
+        player.actions_left += 1
+        discarded_cards = []
+        while len(player.hand) > 0:
+            print(
+                "Your current hand:", 
+                player.hand, 
+                "\nWhat would you like to discard?") #clean up later for nicer ux
+            print(
+                "(Type the card name, 'Cancel' to cancel, or 'Done' to stop discarding.)"
+                )
+            discard = input("> ")
+            if str(discard).lower() == 'cancel': 
+                discarded_cards.append('Cellar')
+                player.hand += discarded_cards #return discarded cards and cellar to hand
+                return
+            elif str(discard).lower() == 'done':
+                continue
+            elif str(discard).capitalize() in player.hand:
+                player.hand.remove(discard)
+                discarded_cards.append(discard)
+            else:
+                print("Sorry, that's not a card in your hand.")
+        player.discard_pile += discarded_cards
+        if len(player.hand) == 0:
+            print("All cards discarded!")
+        print("Drawing {} cards.".format(len(discarded_cards)))
+        player.hand += draw_cards(player, len(discarded_cards))
+        assert len(player.hand) == debug_count
+        return
 
 class Village(Card):
     pass
