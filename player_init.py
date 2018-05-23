09662e4f08015a715e4e298d3cb5321b37b7aab5
David Kiesel-Nield <query42@gmail.com>
2018-05-23 14:39:41 -0700

    Established turn loop and Player methods
    -Moved turn loop and draw/shuffle functions into Player class as methods.
    -Fleshed out turn loop with component methods.
    -Known issues: undefined parse_card() function (and unfinished methods that need to call for it), unfinished buy phase.

--- player_init.py
+++ player_init.py
@@ -2,6 +2,7 @@ import time
 import random
 
 import base_cards
+#import Dominion
 
 
 class Player():
@@ -9,13 +10,127 @@ class Player():
     def __init__(self, name):
         self.name = name
         self.deck = ['Estate', 'Estate', 'Estate', 'Copper', 'Copper', 'Copper', 'Copper', 'Copper', 'Copper', 'Copper']
-        self.hand_size = 5
+        #self.hand_size = 5 currently no effects change this. May only be necessary in _turn_setup()
         self.hand = []
         self.discard_pile = []
         self.turn_count = 0
+
+    def _turn(self):
+        self._turn_setup(self)
+        self._action_phase(self)
+        self._buy_phase(self)
+        return self._cleanup(self) #returns "True" if game end conditions met
+
+    def _turn_setup(self):
+        self.turn_count += 1
+        #resolve duration effects
+        #self.hand_size = 5 #reset from Outpost
         self.actions_left = 1
+        self.money = 0
         self.buys_left = 1
+        self.in_play = []
         self.gained_last_turn = []
+
+    def _action_phase(self):
+        #if no actions in hand, return
+        print("***Action Phase***")
+        while True:
+            if self.actions_left == 0:
+                print("No actions remaining.")
+                break
+            #elif no actions in hand, print "all actions played", return
+            else:
+                self.print_hand(self)
+                print("What action would you like to play? (Type 'None' to play no action)")
+                played_action = input("> ")
+                if played_action.lower() == 'none':
+                    break
+                elif played_action.lower() in self.hand:
+                    #ensure card is an action
+                    self.play_card(self, played_action)
+                    self.do_action(self, played_action)
+                    self.actions_left -= 1
+                else:
+                    print("No such card in hand.")
+
+    def _buy_phase(self):
+        print("***Buy Phase***")
+        #This will need functionality to loop and select treasures to play in the
+        #future (Prosperity, etc.) But for now we can just play all treasures and move on.
+
+        #count player's money
+        #while buys remain:
+            #list cards in supply
+            #prompt player to buy
+            #self.gain_card(self, bought_card)
+        pass
+
+    def _cleanup(self):
+        self.discard_pile += self.in_play
+        self.in_play = []
+        #re: above lines: once duration cards are added, they will need to check each card
+        #and either discard it or move it to a duration zone depending on if it has the 
+        #Duration subtype
+        self.discard_pile += self.hand
+        self.hand = []
+        self.draw_cards(self, 5)
+        #Check for game end. If game ends, return "True"
+        pass
+
+    def play_card(self, played_card):
+        """Remove card from hand and place it in play area."""
+        self.hand.remove(played_card)
+        self.in_play.append(played_card)
+
+    def do_action(self, played_card):
+        """Perform the action associated with playing the named card."""
+        #play the action
+        pass
+
+    def print_hand(self):
+        """Print player's current hand in user-friendly format."""
+        print("Your current hand:")
+        print(self.hand) # rebuild this for nicer ux
+
+    def gain_card(self, gained_card, zone=False):
+        """Gain a card from the supply. By default, add to player's discard pile."""
+        if not zone:
+            zone = self.discard_pile
+        card = parse_card(gained_card)
+        if card.supply == 0:
+            print("The {} supply pile is empty!".format(gained_card))
+            return
+        card.supply -= 1
+        zone.append(gained_card)
+
+    def draw_cards(self, draw_target):
+        """Takes cards from top of deck equal to draw_count and returns them as a list."""
+        deck_cycled = False
+        drawn_cards = []
+        drawn_count = 0
+        for item in range(draw_target):
+            if self.deck:
+                drawn_cards.append(self.deck.pop(0))
+                drawn_count += 1
+            elif self.discard_pile:
+                print("Drew {} card(s): {}.".format(drawn_count, drawn_cards))
+                if deck_cycled:
+                    print("Deck exhausted.")
+                    break
+                else:
+                    print("Shuffling the deck...")
+                    drawn_cards += self._shuffle(self)
+                    deck_cycled = True
+            else:
+                print("No cards remaining in deck!")
+        return drawn_cards
+
+    def _shuffle(self):
+        """Shuffles discard pile (if present) into deck and draws/returns top card."""
+        self.deck += self.discard_pile
+        self.discard_pile = []
+        random.shuffle(self.deck)
+        return self.deck.pop(0)
 
 def player_init():
     """
